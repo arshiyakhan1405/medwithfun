8fece6fb7856d0e60ff273135efd0b035ee5ae3f
arshiyakhan1405 <arshiyawasifkhan@gmail.com>
2026-06-08 13:23:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import React from 'react';
 export default function Home() {
   // Mobile menu open/close karne ke liye React state
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
